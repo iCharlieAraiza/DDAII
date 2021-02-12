50630e888db5ad7333319d2a15bec5f8348234c6
Carlos Araiza <eliptikman1234@gmail.com>
2021-02-12 15:30:53 -0600

    Se agregó la práctica 7

---
+++ Práctica 7/src/com/coding/LectorAchivos.java
@@ -0,0 +1,30 @@
+package com.coding;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class LectorAchivos {
+    String url;
+
+    public LectorAchivos(String url) throws IOException {
+        this.url = url;
+    }
+
+    public void crear(String texto) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(url) );
+        bw.write(texto);
+        bw.close();
+    }
+
+    public String leer() throws IOException {
+        StringBuilder texto = new StringBuilder();
+        String s;
+        BufferedReader br = new BufferedReader( new FileReader(url) );
+
+        while( (s = br.readLine()) != null){
+            texto.append(s);
+        }
+        return texto.toString();
+    }
+
+}
---
+++ Práctica 7/src/com/coding/Main.java
@@ -0,0 +1,14 @@
+package com.coding;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        LectorAchivos archivo = new LectorAchivos("./texto.txt");
+        archivo.crear("Hola! Esta es una prueba para comprobar que funciona");
+        System.out.println( "Leer archivo: " +  archivo.leer() );
+
+    }
+}
